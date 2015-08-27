0e75549e52308c35869093cd0185f50f5827d439
John Dean <john.holmes.dean@gmail.com>
2015-08-26 23:49:28 -0500

    update port

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(function (req, res, next) {
   next();
 });
 
-
-app.listen(process.env.PORT || 5000, function (err, connection) {
+var port = process.env.PORT || 5000;
+app.listen(port, function (err, connection) {
   if (err) throw err;
-  logger.info('Up and running');
+  logger.info('Up and running on ' + port);
   require('./server/db').init(app);
 });

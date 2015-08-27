b3a7380996e0310dc4aeaa6d4c3bcf18ed3b8f3e
John Dean <john.holmes.dean@gmail.com>
2015-08-26 23:24:50 -0500

    update mongo

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongodb: {
-		uri: 'mongodb://mongo/branes'
+		uri: 'mongodb://' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DATABASE
 	}
 };
